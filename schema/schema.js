99d35fe3026d7ff5422b9439acc018be475bae71
David Montesdeoca <backpackerhh@gmail.com>
2021-01-16 20:52:49 +0100

    Allow to get users from JSON server using axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,6 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-const _ = require("lodash");
-
-const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "47", firstName: "Samantha", age: 21 },
-];
+const axios = require("axios");
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -25,7 +20,9 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString },
       },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:4001/users/${args.id}`)
+          .then((response) => response.data);
       },
     },
   },
